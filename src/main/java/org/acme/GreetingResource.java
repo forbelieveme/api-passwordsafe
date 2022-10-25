aca8685c15e8e3d4370ec71cd26fe287b4833c51
forbelieveme <haner.avella@protonmail.com>
2022-10-24 23:15:51 -0500

    Changes url and header for testing

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -28,7 +28,8 @@ public class GreetingResource {
         // URL url = new URL("http://www.javatpoint.com/java-tutorial");
         // HttpURLConnection con = (HttpURLConnection) url.openConnection();
         // con.setRequestMethod("POST");
-        // for (int i = 1; i <= 8; i++) {
+        // con.setRequestProperty("Content-Type", "application/json");
+        // for (int i = 1; i <= 10; i++) {
         // System.out.println(con.getHeaderFieldKey(i) + " = " + con.getHeaderField(i));
         // }
         // con.disconnect();
@@ -37,14 +38,19 @@ public class GreetingResource {
         // }
 
         // POST TEST
+
+        String urlString = "https://lab01vuvm.desabpd.popular.local/beyondtrust/api/public/v3/";
+        String apiKey = "74e592aaec7d75a6b73421a1368c48e68454f9ca0321d8ff2257e1c84192767874dd32c684b449444557e54738504b0ddadc3ffbdcba0afe1db81d88b3f0f3a5";
+        String runAsUser = "_api_GEOPS";
+        String AuthorizationHeader = "PS-Auth key=" + apiKey + ";" + "runas=" + runAsUser + ";";
+
         try {
             String test = "{\"email\":\"Developer5@gmail.com\",\"password\":123456}";
-            URL url = new URL("http://restapi.adequateshop.com/api/authaccount/login");
+            URL url = new URL(urlString);
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
             connection.setRequestMethod("POST");
             connection.setRequestProperty("Content-Type", "application/json");
-            connection.setRequestProperty("Authorization",
-                    "Basic " + Base64.getEncoder().encodeToString(("userName" + ":" + "password").getBytes()));
+            connection.setRequestProperty("Authorization", AuthorizationHeader);
             connection.setDoOutput(true);
             OutputStream outputStream = connection.getOutputStream();
             outputStream.write(test.getBytes());
