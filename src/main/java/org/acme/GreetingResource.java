669dbde76415f8f1f0054075cc1da33c67714d3c
haner-avella <havella@cysce.com>
2022-11-11 05:41:02 -0800

    Adds Content Type and gets the response into a string

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -89,26 +89,30 @@ public class GreetingResource {
             ObjectMapper mapper = new ObjectMapper();
 
             ArrayNode managedAccountArray = (ArrayNode) mapper.readTree(responseStream);
-            JsonNode systemId = managedAccountArray.get(0).get("SystemId");
-            JsonNode accountId = managedAccountArray.get(0).get("AccountId");
+            JsonNode systemId = managedAccountArray.get(4).get("SystemId");
+            JsonNode accountId = managedAccountArray.get(4).get("AccountId");
             System.out.println("SystemId: " + systemId.toString());
             System.out.println("AccountId: " + accountId.toString());
             connection.disconnect();
 
             URL requestIdURL = new URL(baseURL, "Requests");
             connection = (HttpURLConnection) requestIdURL.openConnection();
+			connection.setRequestProperty("Cookie", cookie);
+			connection.setRequestProperty("Content-Type", "application/json; utf-8");
             connection.setRequestProperty("Authorization",
                     "PS-Auth key=57dd0e20bd52bf0178a68ad86ecede1833041f1b6cf58ea258ed529083109415db9d27cf2be0e229a9c977ff2f3f08f908f3c16b79546edd77c317cd660abdf9; runas=salesforceipsa;");
 
-            String requestData = "{\"SystemId\":" + systemId.toString() + ",\"AccountId\":" + accountId.toString()
-                    + ",\"DurationMinutes\":10}";
+            String requestData = "{\"SystemId\":"+systemId+",\"AccountId\":"+accountId+",\"DurationMinutes\":1}";
             connection.setDoOutput(true);
             outputStream = connection.getOutputStream();
             outputStream.write(requestData.getBytes());
             outputStream.close();
             responseStream = connection.getInputStream();
+			byte[] data = new byte[1024];
+			responseStream.read(data);
+			String requestsResponse = new String(data);
 
-            System.out.println("RequestId: " + responseStream.toString());
+            System.out.println("RequestId: " + requestsResponse);
 
             connection.disconnect();
 
