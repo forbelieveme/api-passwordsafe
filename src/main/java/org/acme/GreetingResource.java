25ada02aadf6f0fb508da16c907a08b19e3add19
“haner-avella” <havella@cysce.com>
2022-11-15 17:37:25 -0500

    Formats and organizes code with new variables

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -21,139 +21,113 @@ import com.fasterxml.jackson.databind.node.ArrayNode;
 @Path("/hello")
 public class GreetingResource {
 
-    @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public Response hello() throws IOException {
+        @GET
+        @Produces(MediaType.TEXT_PLAIN)
+        public Response hello() throws IOException {
 
-        // GET TEST
-        // try {
-        // URL url = new URL("http://www.javatpoint.com/java-tutorial");
-        // HttpURLConnection con = (HttpURLConnection) url.openConnection();
-        // con.setRequestMethod("POST");
-        // con.setRequestProperty("Content-Type", "application/json");
-        // for (int i = 1; i <= 10; i++) {
-        // System.out.println(con.getHeaderFieldKey(i) + " = " + con.getHeaderField(i));
-        // }
-        // con.disconnect();
-        // } catch (Exception e) {
-        // System.out.println(e);
-        // }
+                String host = "lab01vuvm.desabpd.popular.local";
+                String apiBase = "/beyondtrust/api/public/v3/";
+                String psAuthKey = "57dd0e20bd52bf0178a68ad86ecede1833041f1b6cf58ea258ed529083109415db9d27cf2be0e229a9c977ff2f3f08f908f3c16b79546edd77c317cd660abdf9";
+                String runAs = "salesforceipsa";
+                String authorizationHeader = "PS-Auth key=" + psAuthKey + "; runas=" + runAs + ";";
+                URL baseURL = new URL("HTTPS", host, 443, apiBase);
+                HttpURLConnection connection;
+                String requestDataString = "";
+                String cookieString = "";
+                OutputStream outputStream;
+                InputStream responseStream;
 
-        // POST TEST
+                try {
+                        /*
+                         **********
+                         **********
+                         */
+                        URL SignAppInURL = new URL(baseURL, "Auth/SignAppIn");
+                        connection = (HttpURLConnection) SignAppInURL.openConnection();
+                        connection.setRequestProperty("Authorization", authorizationHeader);
+                        connection.setDoOutput(true);
+                        outputStream = connection.getOutputStream();
+                        outputStream.write(requestDataString.getBytes());
+                        outputStream.close();
+                        connection.getInputStream();
+                        cookieString = connection.getHeaderFields().get("Set-Cookie").get(0);
+                        connection.disconnect();
 
-        // String baseURL =
-        // "https://lab01vuvm.desabpd.popular.local/beyondtrust/api/public/v3/";
-        // String apiKey =
-        // "74e592aaec7d75a6b73421a1368c48e68454f9ca0321d8ff2257e1c84192767874dd32c684b449444557e54738504b0ddadc3ffbdcba0afe1db81d88b3f0f3a5";
-        // String runAsUser = "_api_GEOPS";
-        // String AuthorizationHeader1 = "PS-Auth key=" + apiKey + ";";
-        // String AuthorizationHeader2 = "runas=" + runAsUser + ";";
-        // javax.net.ssl.HttpsURLConnection.setDefaultHostnameVerifier(
-        // new javax.net.ssl.HostnameVerifier() {
-        // public boolean verify(String hostname,
-        // javax.net.ssl.SSLSession sslSession) {
-        // return hostname.equals("10.96.36.15");
-        // }
-        // });
-        // HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        // connection.setRequestMethod("POST");
-        // connection.setRequestProperty("Content-Type", "application/json");
-        // connection.setRequestProperty("Authorization",
-        // "PS-Auth
-        // key=74e592aaec7d75a6b73421a1368c48e68454f9ca0321d8ff2257e1c84192767874dd32c684b449444557e54738504b0ddadc3ffbdcba0afe1db81d88b3f0f3a5;
-        // runas=_api_GEOPS;");
-        // connection.setRequestProperty("Content-Length", String.valueOf(5));
+                        /*
+                         **********
+                         **********
+                         */
+                        URL managedAccountsURL = new URL(baseURL, "ManagedAccounts");
+                        connection = (HttpURLConnection) managedAccountsURL.openConnection();
+                        connection.setRequestProperty("Cookie", cookieString);
+                        connection.setRequestProperty("Authorization", authorizationHeader);
+                        responseStream = connection.getInputStream();
+                        ObjectMapper mapper = new ObjectMapper();
+                        ArrayNode managedAccountArray = (ArrayNode) mapper.readTree(responseStream);
+                        JsonNode systemId = managedAccountArray.get(4).get("SystemId");
+                        JsonNode accountId = managedAccountArray.get(4).get("AccountId");
+                        System.out.println("SystemId: " + systemId.toString());
+                        System.out.println("AccountId: " + accountId.toString());
+                        connection.disconnect();
 
-        try {
-            URL baseURL = new URL("HTTPS", "lab01vuvm.desabpd.popular.local", 443, "/beyondtrust/api/public/v3/");
-            URL SignAppInURL = new URL(baseURL, "Auth/SignAppIn");
-            HttpURLConnection connection = (HttpURLConnection) SignAppInURL.openConnection();
-            connection.setRequestProperty("Authorization",
-                    "PS-Auth key=57dd0e20bd52bf0178a68ad86ecede1833041f1b6cf58ea258ed529083109415db9d27cf2be0e229a9c977ff2f3f08f908f3c16b79546edd77c317cd660abdf9; runas=salesforceipsa;");
-            String test = "";
-            connection.setDoOutput(true);
-            OutputStream outputStream = connection.getOutputStream();
-            outputStream.write(test.getBytes());
-            outputStream.close();
-            connection.getInputStream();
-            String cookie = connection.getHeaderFields().get("Set-Cookie").get(0);
-            System.out.println(cookie);
-            connection.disconnect();
+                        /*
+                         **********
+                         **********
+                         */
+                        URL requestIdURL = new URL(baseURL, "Requests");
+                        connection = (HttpURLConnection) requestIdURL.openConnection();
+                        connection.setRequestProperty("Cookie", cookieString);
+                        connection.setRequestProperty("Content-Type", "application/json; utf-8");
+                        connection.setRequestProperty("Authorization", authorizationHeader);
 
-            URL managedAccountsURL = new URL(baseURL, "ManagedAccounts");
-            connection = (HttpURLConnection) managedAccountsURL.openConnection();
-            connection.setRequestProperty("Cookie", cookie);
-            connection.setRequestProperty("Authorization",
-                    "PS-Auth key=57dd0e20bd52bf0178a68ad86ecede1833041f1b6cf58ea258ed529083109415db9d27cf2be0e229a9c977ff2f3f08f908f3c16b79546edd77c317cd660abdf9; runas=salesforceipsa;");
-            InputStream responseStream = connection.getInputStream();
-            ObjectMapper mapper = new ObjectMapper();
+                        requestDataString = "{\"SystemId\":" + systemId + ",\"AccountId\":" + accountId
+                                        + ",\"DurationMinutes\":1}";
+                        connection.setDoOutput(true);
+                        outputStream = connection.getOutputStream();
+                        outputStream.write(requestDataString.getBytes());
+                        outputStream.close();
+                        responseStream = connection.getInputStream();
+                        byte[] data = new byte[1024];
+                        responseStream.read(data);
+                        String requestsResponse = new String(data);
+                        System.out.println("RequestId: " + requestsResponse);
+                        connection.disconnect();
 
-            ArrayNode managedAccountArray = (ArrayNode) mapper.readTree(responseStream);
-            JsonNode systemId = managedAccountArray.get(4).get("SystemId");
-            JsonNode accountId = managedAccountArray.get(4).get("AccountId");
-            System.out.println("SystemId: " + systemId.toString());
-            System.out.println("AccountId: " + accountId.toString());
-            connection.disconnect();
+                        /*
+                         **********
+                         **********
+                         */
+                        URL credentialsURL = new URL(baseURL, "Credentials/" + requestsResponse);
+                        connection = (HttpURLConnection) credentialsURL.openConnection();
+                        connection.setRequestProperty("Cookie", cookieString);
+                        connection.setRequestProperty("Authorization", authorizationHeader);
+                        responseStream = connection.getInputStream();
+                        byte[] responseData = new byte[1024];
+                        responseStream.read(responseData);
+                        String credentialsResponse = new String(responseData);
+                        System.out.println("Credentials: " + credentialsResponse);
+                        connection.disconnect();
 
-            URL requestIdURL = new URL(baseURL, "Requests");
-            connection = (HttpURLConnection) requestIdURL.openConnection();
-            connection.setRequestProperty("Cookie", cookie);
-            connection.setRequestProperty("Content-Type", "application/json; utf-8");
-            connection.setRequestProperty("Authorization",
-                    "PS-Auth key=57dd0e20bd52bf0178a68ad86ecede1833041f1b6cf58ea258ed529083109415db9d27cf2be0e229a9c977ff2f3f08f908f3c16b79546edd77c317cd660abdf9; runas=salesforceipsa;");
+                        /*
+                         **********
+                         **********
+                         */
+                        URL SignOutURL = new URL(baseURL, "Auth/Signout");
+                        connection = (HttpURLConnection) SignOutURL.openConnection();
+                        connection.setRequestProperty("Cookie", cookieString);
+                        connection.setRequestProperty("Authorization", authorizationHeader);
+                        requestDataString = "";
+                        connection.setDoOutput(true);
+                        outputStream = connection.getOutputStream();
+                        outputStream.write(requestDataString.getBytes());
+                        outputStream.close();
+                        connection.getInputStream();
+                        connection.disconnect();
 
-            String requestData = "{\"SystemId\":" + systemId + ",\"AccountId\":" + accountId
-                    + ",\"DurationMinutes\":1}";
-            connection.setDoOutput(true);
-            outputStream = connection.getOutputStream();
-            outputStream.write(requestData.getBytes());
-            outputStream.close();
-            responseStream = connection.getInputStream();
-            byte[] data = new byte[1024];
-            responseStream.read(data);
-            String requestsResponse = new String(data);
+                } catch (Exception e) {
+                        System.out.println(e);
+                }
 
-            System.out.println("RequestId: " + requestsResponse);
-
-            connection.disconnect();
-
-            URL credentialsURL = new URL(baseURL, "Credentials/" + requestsResponse);
-            connection = (HttpURLConnection) credentialsURL.openConnection();
-            connection.setRequestProperty("Cookie", cookie);
-            connection.setRequestProperty("Authorization",
-                    "PS-Auth key=57dd0e20bd52bf0178a68ad86ecede1833041f1b6cf58ea258ed529083109415db9d27cf2be0e229a9c977ff2f3f08f908f3c16b79546edd77c317cd660abdf9; runas=salesforceipsa;");
-            responseStream = connection.getInputStream();
-            byte[] data2 = new byte[1024];
-            responseStream.read(data2);
-            String credentialsResponse = new String(data2);
-            System.out.println("Credentials: " + credentialsResponse);
-            connection.disconnect();
-
-            URL SignOutURL = new URL(baseURL, "Auth/Signout");
-            connection = (HttpURLConnection) SignOutURL.openConnection();
-            connection.setRequestProperty("Cookie", cookie);
-
-            connection.setRequestProperty("Authorization",
-                    "PS-Auth key=57dd0e20bd52bf0178a68ad86ecede1833041f1b6cf58ea258ed529083109415db9d27cf2be0e229a9c977ff2f3f08f908f3c16b79546edd77c317cd660abdf9; runas=salesforceipsa;");
-            String test2 = "";
-            connection.setDoOutput(true);
-            outputStream = connection.getOutputStream();
-            outputStream.write(test2.getBytes());
-            outputStream.close();
-            connection.getInputStream();
-            connection.disconnect();
-
-        } catch (Exception e) {
-            System.out.println(e);
+                return Response.ok().build();
         }
-
-        return Response.ok().build();
-    }
 }
-
-// URL baseURL = new URL("HTTPS", "the-server", 443,
-// "/BeyondTrust/api/public/v3/");
-// URL url = new URL(baseURL, "Auth/SignAppIn");
-// HttpURLConnection conn = (HttpURLConnection)url.openConnection();
-// conn.setRequestProperty("Authorization","PS-Auth key=c479a66f…c9484d;
-// runas=doe-main\johndoe;");
