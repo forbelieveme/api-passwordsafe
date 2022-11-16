c84bbff453c353c8ac3eb9930d076e41aba80e59
forbelieveme <haner.avella@protonmail.com>
2022-11-15 21:38:52 -0500

    Adds the X-Forwarded-For headers to the requests

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -5,9 +5,7 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.Base64;
 
-import javax.net.ssl.HttpsURLConnection;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -23,129 +21,132 @@ public class GreetingResource {
 
         @GET
         @Produces(MediaType.TEXT_PLAIN)
-        public Response hello()  {
-
-                String host = "lab01vuvm.desabpd.popular.local";
+        public Response hello() {
+                /* Desarrollo */
+                String host = "apipam-lab.desabpd.popular.local";
+                /* QA */
+                // String host = "apipam-qa.qaintdom.local";
+                /* Produccion */
+                // String host = "apipam.corp.popular.local";
                 String apiBase = "/beyondtrust/api/public/v3/";
                 String psAuthKey = "57dd0e20bd52bf0178a68ad86ecede1833041f1b6cf58ea258ed529083109415db9d27cf2be0e229a9c977ff2f3f08f908f3c16b79546edd77c317cd660abdf9";
                 String runAs = "salesforceipsa";
-                
-				try{
-					System.out.println(passwordSafeCredentials(host, apiBase, psAuthKey, runAs));
-				} catch (Exception e) {
+
+                try {
+                        System.out.println(passwordSafeCredentials(host, apiBase, psAuthKey, runAs));
+                } catch (Exception e) {
                         System.out.println(e);
                 }
-                
-                 
 
                 return Response.ok().build();
         }
-		
-		public String passwordSafeCredentials(String host, String apiBase, String psAuthKey, String runAs) throws IOException {
-			String authorizationHeader = "PS-Auth key=" + psAuthKey + "; runas=" + runAs + ";";
+
+        public String passwordSafeCredentials(String host, String apiBase, String psAuthKey, String runAs)
+                        throws IOException {
+                String authorizationHeader = "PS-Auth key=" + psAuthKey + "; runas=" + runAs + ";";
                 URL baseURL = new URL("HTTPS", host, 443, apiBase);
                 HttpURLConnection connection;
                 String requestDataString = "";
                 String cookieString = "";
                 OutputStream outputStream;
                 InputStream responseStream;
-				String credentialsResponse = "";
+                String credentialsResponse = "";
 
-                
-                        /*
-                         **********
-                         **********
-                         */
-                        URL SignAppInURL = new URL(baseURL, "Auth/SignAppIn");
-                        connection = (HttpURLConnection) SignAppInURL.openConnection();
-                        connection.setRequestProperty("Authorization", authorizationHeader);
-                        connection.setDoOutput(true);
-                        outputStream = connection.getOutputStream();
-                        outputStream.write(requestDataString.getBytes());
-                        outputStream.close();
-                        connection.getInputStream();
-                        cookieString = connection.getHeaderFields().get("Set-Cookie").get(0);
-                        connection.disconnect();
+                /*
+                 **********
+                 **********
+                 */
+                URL SignAppInURL = new URL(baseURL, "Auth/SignAppIn");
+                connection = (HttpURLConnection) SignAppInURL.openConnection();
+                // connection.setRequestProperty("X-Forwarded-For", "");
+                connection.setRequestProperty("Authorization", authorizationHeader);
+                connection.setDoOutput(true);
+                outputStream = connection.getOutputStream();
+                outputStream.write(requestDataString.getBytes());
+                outputStream.close();
+                connection.getInputStream();
+                cookieString = connection.getHeaderFields().get("Set-Cookie").get(0);
+                connection.disconnect();
 
-                        /*
-                         **********
-                         **********
-                         */
-                        URL managedAccountsURL = new URL(baseURL, "ManagedAccounts");
-                        connection = (HttpURLConnection) managedAccountsURL.openConnection();
-                        connection.setRequestProperty("Cookie", cookieString);
-                        connection.setRequestProperty("Authorization", authorizationHeader);
-                        responseStream = connection.getInputStream();
-                        ObjectMapper mapper = new ObjectMapper();
-                        ArrayNode managedAccountArray = (ArrayNode) mapper.readTree(responseStream);
-                        JsonNode systemId = null;
-                        JsonNode accountId = null;
-						
-						for (JsonNode jsonNode : managedAccountArray) {
-                                if (jsonNode.get("AccountName").asText().equals("_IPSACRM")) {
-                                        System.out.println(jsonNode.get("UserPrincipalName").asText());
-										systemId=jsonNode.get("SystemId");
-										accountId=jsonNode.get("AccountId");
-                                }
+                /*
+                 **********
+                 **********
+                 */
+                URL managedAccountsURL = new URL(baseURL, "ManagedAccounts");
+                connection = (HttpURLConnection) managedAccountsURL.openConnection();
+                connection.setRequestProperty("Cookie", cookieString);
+                // connection.setRequestProperty("X-Forwarded-For", "");
+                connection.setRequestProperty("Authorization", authorizationHeader);
+                responseStream = connection.getInputStream();
+                ObjectMapper mapper = new ObjectMapper();
+                ArrayNode managedAccountArray = (ArrayNode) mapper.readTree(responseStream);
+                JsonNode systemId = null;
+                JsonNode accountId = null;
+
+                for (JsonNode jsonNode : managedAccountArray) {
+                        if (jsonNode.get("AccountName").asText().equals("_IPSACRM")) {
+                                System.out.println(jsonNode.get("UserPrincipalName").asText());
+                                systemId = jsonNode.get("SystemId");
+                                accountId = jsonNode.get("AccountId");
                         }
-                        System.out.println("SystemId: " + systemId.toString());
-                        System.out.println("AccountId: " + accountId.toString());
-                        connection.disconnect();
+                }
+                connection.disconnect();
 
-                        /*
-                         **********
-                         **********
-                         */
-                        URL requestIdURL = new URL(baseURL, "Requests");
-                        connection = (HttpURLConnection) requestIdURL.openConnection();
-                        connection.setRequestProperty("Cookie", cookieString);
-                        connection.setRequestProperty("Content-Type", "application/json; utf-8");
-                        connection.setRequestProperty("Authorization", authorizationHeader);
+                /*
+                 **********
+                 **********
+                 */
+                URL requestIdURL = new URL(baseURL, "Requests");
+                connection = (HttpURLConnection) requestIdURL.openConnection();
+                connection.setRequestProperty("Cookie", cookieString);
+                // connection.setRequestProperty("X-Forwarded-For", "");
+                connection.setRequestProperty("Content-Type", "application/json; utf-8");
+                connection.setRequestProperty("Authorization", authorizationHeader);
 
-                        requestDataString = "{\"SystemId\":" + systemId + ",\"AccountId\":" + accountId
-                                        + ",\"DurationMinutes\":1}";
-                        connection.setDoOutput(true);
-                        outputStream = connection.getOutputStream();
-                        outputStream.write(requestDataString.getBytes());
-                        outputStream.close();
-                        responseStream = connection.getInputStream();
-                        byte[] data = new byte[1024];
-                        responseStream.read(data);
-                        String requestsResponse = new String(data);
-                        System.out.println("RequestId: " + requestsResponse);
-                        connection.disconnect();
+                requestDataString = "{\"SystemId\":" + systemId + ",\"AccountId\":" + accountId
+                                + ",\"DurationMinutes\":1}";
+                connection.setDoOutput(true);
+                outputStream = connection.getOutputStream();
+                outputStream.write(requestDataString.getBytes());
+                outputStream.close();
+                responseStream = connection.getInputStream();
+                byte[] data = new byte[1024];
+                responseStream.read(data);
+                String requestsResponse = new String(data);
+                connection.disconnect();
 
-                        /*
-                         **********
-                         **********
-                         */
-                        URL credentialsURL = new URL(baseURL, "Credentials/" + requestsResponse);
-                        connection = (HttpURLConnection) credentialsURL.openConnection();
-                        connection.setRequestProperty("Cookie", cookieString);
-                        connection.setRequestProperty("Authorization", authorizationHeader);
-                        responseStream = connection.getInputStream();
-                        byte[] responseData = new byte[1024];
-                        responseStream.read(responseData);
-                        credentialsResponse = new String(responseData);
-                        System.out.println("Credentials: " + credentialsResponse);
-                        connection.disconnect();
+                /*
+                 **********
+                 **********
+                 */
+                URL credentialsURL = new URL(baseURL, "Credentials/" + requestsResponse);
+                connection = (HttpURLConnection) credentialsURL.openConnection();
+                connection.setRequestProperty("Cookie", cookieString);
+                // connection.setRequestProperty("X-Forwarded-For", "");
+                connection.setRequestProperty("Authorization", authorizationHeader);
+                responseStream = connection.getInputStream();
+                byte[] responseData = new byte[1024];
+                responseStream.read(responseData);
+                credentialsResponse = new String(responseData);
+                connection.disconnect();
 
-                        /*
-                         **********
-                         **********
-                         */
-                        URL SignOutURL = new URL(baseURL, "Auth/Signout");
-                        connection = (HttpURLConnection) SignOutURL.openConnection();
-                        connection.setRequestProperty("Cookie", cookieString);
-                        connection.setRequestProperty("Authorization", authorizationHeader);
-                        requestDataString = "";
-                        connection.setDoOutput(true);
-                        outputStream = connection.getOutputStream();
-                        outputStream.write(requestDataString.getBytes());
-                        outputStream.close();
-                        connection.getInputStream();
-				connection.disconnect();
-				
-				return credentialsResponse;
-		}
+                /*
+                 **********
+                 **********
+                 */
+                URL SignOutURL = new URL(baseURL, "Auth/Signout");
+                connection = (HttpURLConnection) SignOutURL.openConnection();
+                connection.setRequestProperty("Cookie", cookieString);
+                // connection.setRequestProperty("X-Forwarded-For", "");
+                connection.setRequestProperty("Authorization", authorizationHeader);
+                requestDataString = "";
+                connection.setDoOutput(true);
+                outputStream = connection.getOutputStream();
+                outputStream.write(requestDataString.getBytes());
+                outputStream.close();
+                connection.getInputStream();
+                connection.disconnect();
+
+                return credentialsResponse;
+        }
 }
